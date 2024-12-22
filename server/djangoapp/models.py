6b383fed46916103563ca3efad3bf237314910e6
Dacosmicgiant <vedantvanpro@gmail.com>
2024-12-22 16:52:58 -0500

    module 3 part 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,37 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+# CarMake model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # You can add other fields as needed, like a logo, country of origin, etc.
+    
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# CarModel model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # You can add more types here
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    
+    # You can add other fields such as engine type, color options, etc.
+    
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"  # Return both car make and car model as string representation
