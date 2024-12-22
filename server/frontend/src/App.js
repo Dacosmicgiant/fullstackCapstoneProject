d216b8bcd4e7d2a3c152668451725f166af60b09
Dacomicgiant <vedantvanpro@gmail.com>
2024-12-22 15:13:00 -0500

    implemented user management with REACT frontend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,13 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import Register from './components/Register/Register'; // adjust the path based on where your Register component is located
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
